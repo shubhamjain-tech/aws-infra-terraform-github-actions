cd34f2baf4ece3a79b8fa4e6dcbc77e6fbeb7dc7
shubhamjain-tech <shubhamjain9005@outlook.com>
2026-06-08 15:54:21 +0530

    Refactor Terraform configurations for EC2, S3, and VPC modules; add .gitattributes for consistent line endings

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,42 +1,44 @@
 module "ec2" {
-  source      = "../../modules/ec2"
+  source = "../../modules/ec2"
+
   instances = {
-  "dev-instance-1" = {
-    ami_id        = "ami-0dee22c13ea7a9a67", 
-    instance_type = "t3.micro", 
-    name          = "dev-instance-1", 
-    subnet_id     = module.vpc.public_subnet_ids[0]
-  },
-  "dev-instance-2" = {
-    ami_id        = "ami-0dee22c13ea7a9a67", 
-    instance_type = "t3.micro", 
-    name          = "dev-instance-2", 
-    subnet_id     = module.vpc.public_subnet_ids[1]
+    "dev-instance-1" = {
+      ami_id        = "ami-0dee22c13ea7a9a67"
+      instance_type = "t3.micro"
+      name          = "dev-instance-1"
+      subnet_id     = module.vpc.public_subnet_ids[0]
+    },
+    "dev-instance-2" = {
+      ami_id        = "ami-0dee22c13ea7a9a67"
+      instance_type = "t3.micro"
+      name          = "dev-instance-2"
+      subnet_id     = module.vpc.public_subnet_ids[1]
+    }
   }
-}
+
   environment = "dev"
 }
 
-
-
 module "s3" {
-  source      = "../../modules/s3"
+  source = "../../modules/s3"
+
   bucket_names = {
     "bucket1" = { bucket_name = "dev-demo-buck-6721" },
     "bucket2" = { bucket_name = "dev-demo-buck-1241" },
     "bucket3" = { bucket_name = "dev-demo-buck-9021" },
     "bucket4" = { bucket_name = "dev-demo-buck-294" }
   }
+
   environment = "dev"
 }
 
 module "vpc" {
-  source                = "../../modules/vpc"
-  vpc_cidr              = "10.0.0.0/16"
-  vpc_name              = "my-vpc"
-  public_subnet_cidrs   = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidrs  = ["10.0.3.0/24", "10.0.4.0/24"]
-  availability_zones    = ["ap-south-1a", "ap-south-1b"]
-  
+  source               = "../../modules/vpc"
+  vpc_cidr             = "10.0.0.0/16"
+  vpc_name             = "my-vpc"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones   = ["ap-south-1a", "ap-south-1b"]
+
   environment = "dev"
 }
--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,41 +1,44 @@
 module "ec2" {
-  source      = "../../modules/ec2"
+  source = "../../modules/ec2"
+
   instances = {
-  "prod-instance-1" = {
-    ami_id        = "ami-0dee22c13ea7a9a67", 
-    instance_type = "t3.micro", 
-    name          = "prod-instance-1", 
-    subnet_id     = module.vpc.public_subnet_ids[0]
-  },
-  "prod-instance-2" = {
-    ami_id        = "ami-0dee22c13ea7a9a67", 
-    instance_type = "t3.micro", 
-    name          = "prod-instance-2", 
-    subnet_id     = module.vpc.public_subnet_ids[1]
+    "prod-instance-1" = {
+      ami_id        = "ami-0dee22c13ea7a9a67"
+      instance_type = "t3.micro"
+      name          = "prod-instance-1"
+      subnet_id     = module.vpc.public_subnet_ids[0]
+    },
+    "prod-instance-2" = {
+      ami_id        = "ami-0dee22c13ea7a9a67"
+      instance_type = "t3.micro"
+      name          = "prod-instance-2"
+      subnet_id     = module.vpc.public_subnet_ids[1]
+    }
   }
-}
+
   environment = "prod"
 }
 
-
-
 module "s3" {
-  source      = "../../modules/s3"
+  source = "../../modules/s3"
+
   bucket_names = {
     "bucket1" = { bucket_name = "prod-demo-buck-6721" },
     "bucket2" = { bucket_name = "prod-demo-buck-1241" },
     "bucket3" = { bucket_name = "prod-demo-buck-9021" },
     "bucket4" = { bucket_name = "prod-demo-buck-294" }
   }
+
   environment = "prod"
 }
 
 module "vpc" {
-  source                = "../../modules/vpc"
-  vpc_cidr              = "10.0.0.0/16"
-  vpc_name              = "my-vpc"
-  public_subnet_cidrs   = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidrs  = ["10.0.3.0/24", "10.0.4.0/24"]
-  availability_zones    = ["ap-south-1a", "ap-south-1b"]
+  source               = "../../modules/vpc"
+  vpc_cidr             = "10.0.0.0/16"
+  vpc_name             = "my-vpc"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones   = ["ap-south-1a", "ap-south-1b"]
+
   environment = "prod"
 }
