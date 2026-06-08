def3ec7b92cc51c17dd74b9a2c4fbcd7b6d10e11
shubhamjain-tech <shubhamjain9005@outlook.com>
2026-06-08 15:34:11 +0530

    Update S3 bucket name for backend configuration in dev and prod environments

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "aws-terraform-state-file-2026"
+    bucket         = "aws-terraform-state-file-2026-848504403730"
     key            = "prod/terraform.tfstate"
     region         = "ap-south-1"
     use_lockfile   = true
