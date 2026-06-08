dd84136ad96d3e442338b9aa4679d3e4cf8bceb7
shubhamjain-tech <shubhamjain9005@outlook.com>
2026-06-08 15:10:57 +0530

    Add Terraform CI/CD workflow and update backend configurations for dev and prod environments

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "mybucket-backend-6721"
+    bucket         = "aws-terraform-state-file-2026"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
