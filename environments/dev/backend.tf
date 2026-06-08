618e7f3a40e7f04501e140af08f7ade325611fc3
shubhamjain-tech <shubhamjain9005@outlook.com>
2026-06-08 16:00:54 +0530

    Format backend configuration for S3 in dev and prod environments for consistency

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "aws-terraform-state-file-2026-848504403730"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    use_lockfile   = true
-    encrypt        = true
+    bucket       = "aws-terraform-state-file-2026-848504403730"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
